352a0a2f8324ddca4523dec38420bc78b3bf4d6b
LADOSSIFPB <ladossifpb@gmail.com>
2016-07-12 08:43:46 -0300

    Uri Intente
    - Abrir browser

--- MinhaAppNoGit/app/src/main/java/br/edu/ifpb/minhaappnogit/MainActivity.java
+++ MinhaAppNoGit/app/src/main/java/br/edu/ifpb/minhaappnogit/MainActivity.java
@@ -1,7 +1,11 @@
 package br.edu.ifpb.minhaappnogit;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirBrowser(View view) {
+
+        EditText urlEditText = (EditText) findViewById(R.id.urlEditText);
+        Uri uri = Uri.parse(urlEditText.getText().toString());
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+        startActivity(intent);
     }
 }
